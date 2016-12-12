c28bb45936b30431c7b715449fdb61b911f9c970
ericyang <475289868@qq.com>
2016-12-12 15:47:03 +0800

    fourth commit

--- app/src/main/java/com/example/yang/module/MainActivity.java
+++ app/src/main/java/com/example/yang/module/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.yang.module;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbAccessory;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private UsbManager usbManager;
+    private UsbAccessory usbAccessory;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
     }
 }
